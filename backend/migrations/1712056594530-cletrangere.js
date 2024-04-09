0554454fa7fb829102b5fa608ed32dd2968597ee
TheFox272 <fabien.girard27@gmail.com>
2024-04-09 12:26:26 +0200

    added migrations

---
+++ backend/migrations/1712056594530-cletrangere.js
@@ -0,0 +1,29 @@
+import typeorm from "typeorm";
+
+const { MigrationInterface, QueryRunner } = typeorm;
+
+export default class Cletrangere1712056594530 {
+    name = 'Cletrangere1712056594530'
+
+    async up(queryRunner) {
+        await queryRunner.query(`
+            ALTER TABLE "comment"
+            ALTER COLUMN "movieId" DROP NOT NULL
+        `);
+        await queryRunner.query(`
+            ALTER TABLE "comment"
+            ADD CONSTRAINT "FK_aea4918c888422550a85e257894" FOREIGN KEY ("movieId") REFERENCES "movie"("id") ON DELETE CASCADE ON UPDATE NO ACTION
+        `);
+    }
+
+    async down(queryRunner) {
+        await queryRunner.query(`
+            ALTER TABLE "comment" DROP CONSTRAINT "FK_aea4918c888422550a85e257894"
+        `);
+        await queryRunner.query(`
+            ALTER TABLE "comment"
+            ALTER COLUMN "movieId"
+            SET NOT NULL
+        `);
+    }
+}
