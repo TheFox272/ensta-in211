0708de9de906495a1ea62b169b6b4a2fdcb9c5ad
Mathiaz21 <mathias.gilbert42@gmail.com>
2024-04-23 10:10:18 +0200

    Finalisation du bouton stylé

--- frontend/src/components/sliderButton/SliderButton.jsx
+++ frontend/src/components/sliderButton/SliderButton.jsx
@@ -4,6 +4,7 @@ import React from 'react'
 export const SliderButton = ({clickFunction, label}) => {
   return (
     <button onClick={clickFunction} className="sliderButton">
+      <div className="background-slider"></div>
         <span className="label">{label}</span>
         </button>
   )
